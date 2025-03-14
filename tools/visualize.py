1675e71b48d7c9730d9765a3c19287ce68d56bc2
Enes Özipek <enes.ozipek@topdatascience.com>
2025-03-14 10:10:41 +0200

    Visualization script fixed

--- tools/visualize.py
+++ tools/visualize.py
@@ -1,26 +1,35 @@
 import os
 import tqdm
 import json
+import argparse
 from visual_nuscenes import NuScenes
-use_gt = False
-out_dir = './result_vis/'
-result_json = "test/repdetr3d_eva02_800_bs2_seq_24e/Thu_Mar__6_15_22_07_2025/pts_bbox/results_nusc"
-dataroot='v1.0-mini'
-if not os.path.exists(out_dir):
-    os.mkdir(out_dir)
 
-if use_gt:
-    nusc = NuScenes(version='v1.0-mini', dataroot=dataroot, verbose=True, pred = False, annotations = "sample_annotation")
-else:
-    nusc = NuScenes(version='v1.0-mini', dataroot=dataroot, verbose=True, pred = True, annotations = result_json, score_thr=0.25)
+def main(use_gt, out_dir, result_json, dataroot, version):
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
 
-with open('{}.json'.format(result_json)) as f:
-    table = json.load(f)
-tokens = list(table['results'].keys())
-
-for token in tqdm.tqdm(tokens[:100]):
     if use_gt:
-        nusc.render_sample(token, out_path = "./result_vis/"+token+"_gt.png", verbose=False)
+        nusc = NuScenes(version=version, dataroot=dataroot, verbose=True, pred=False, annotations="sample_annotation")
     else:
-        nusc.render_sample(token, out_path = "./result_vis/"+token+"_pred.png", verbose=False)
+        nusc = NuScenes(version=version, dataroot=dataroot, verbose=True, pred=True, annotations=result_json, score_thr=0.25)
+
+    with open('{}.json'.format(result_json)) as f:
+        table = json.load(f)
+    tokens = list(table['results'].keys())
+
+    for token in tqdm.tqdm(tokens[:100]):
+        if use_gt:
+            nusc.render_sample(token, out_path=os.path.join(out_dir, token + "_gt.png"), verbose=False)
+        else:
+            nusc.render_sample(token, out_path=os.path.join(out_dir, token + "_pred.png"), verbose=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Visualize NuScenes data")
+    parser.add_argument('--use_gt', action='store_true', help='Use ground truth annotations')
+    parser.add_argument('--out_dir', type=str, default='./result_vis/', help='Output directory for visualizations')
+    parser.add_argument('--result_json', type=str, required=True, help='Path to the result JSON file')
+    parser.add_argument('--dataroot', type=str, default='data/nuscenes', help='NuScenes data root directory')
+    parser.add_argument('--version', type=str, default='v1.0-mini', help='NuScenes dataset version')
+    args = parser.parse_args()
+    main(args.use_gt, args.out_dir, args.result_json, args.dataroot, args.version)
 
